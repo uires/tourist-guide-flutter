3337f11074e02c2f8b39f6a06cb229a6085aa535
uíres <uiresdeivide1@gmail.com>
2020-01-19 21:07:41 -0300

    text section e divisão dos widgets

---
+++ lib/screens/location_detail/text_section.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TextSection extends StatelessWidget {
+
+  final Color _color;
+
+  TextSection(this._color);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      decoration: BoxDecoration(
+        color: this._color
+        ),
+        child: Text('hi')
+    );
+  }
+}

d02c21b5ef67e84716664fd6c829406989e82d38
uíres <uiresdeivide1@gmail.com>
2020-01-26 17:06:52 -0300

    adicionando modelo de dados

---
+++ lib/models/location.dart
@@ -0,0 +1,11 @@
+import 'location_fact.dart';
+
+class Location {
+
+  String name;
+  String imagePath;
+  final List<LocationFact> facts;
+
+  Location(this.name, this.imagePath, this.facts);
+
+}

861a876dcce1d84d769561d4d6f98f596841d914
uíres <uiresdeivide1@gmail.com>
2020-01-26 17:26:42 -0300

    organização de lista com dart e adaptação do modelo de dado para os widgets

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
+import '../../models/location.dart';
 
 class LocationDetail extends StatelessWidget {
-
+  
   @override
   Widget build(BuildContext context) {
+    
+    final locations = Location.fetchAll();
+    final location = locations.first;
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Hello'),
-        ),
-        body: Column(mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ImageBanner("assets/images/kiyomizu-dera.jpg"),
-            TextSection('summary', 'somthing 1'),
-            TextSection('summary', 'somthing 2'),
-            TextSection('summary', 'somthing 3'),
-        ]
+      appBar: AppBar(
+        title: Text(location.name),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ImageBanner(location.imagePath),
+        ]..addAll(this.textSections(location))
       )
     );
+  }
+
+  List<Widget> textSections(Location location) {
+    
+    return location.facts.map((fact) => TextSection(fact.title, fact.text)).toList();
   }
 }
